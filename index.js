0366d777176c6ba802e8ab3d0ff7796ddd8c27c1
Pedro Espinola <pedrohgespinola@live.com>
2021-09-02 22:51:16 -0300

    att js

--- index.js
+++ index.js
@@ -3,25 +3,32 @@ const botao = document.getElementById("butao")
 botao.addEventListener("click", function(e){
     e.preventDefault()
    let codificar = document.getElementById("inText").value.toLocaleUpperCase().split("")
-   let deslocamento = document.getElementById("numDesl").value
+   let deslocamento = document.getElementById("numDesl").value 
+   let numbDesloc = parseInt(deslocamento)
    let retorno = []
-   let numerofinal
+   let numeroFinal
 
    for (let i = 0; i < codificar.length; i++){
        let letras = codificar[i]
        let transform = parseInt(letras.charCodeAt(0))
 
        if (transform===32){
-        numerofinal = transform
+        numeroFinal = transform
        }
        else {
-           numerofinal = transform + deslocamento
+           numeroFinal = transform + numbDesloc
        }
 
-       if (numerofinal > 90){
-           numerofinal -=26
+       if (numeroFinal > 90){
+           numeroFinal -=26
        }
+
+       let resultado = String.fromCharCode(numeroFinal)
+       retorno.push(resultado)
    }
    
+  let textCodificado = document.getElementById("result")
+    textCodificado.innerText = retorno.join("")
+    textCodificado.classList.remove("invisivel")
    
 })
